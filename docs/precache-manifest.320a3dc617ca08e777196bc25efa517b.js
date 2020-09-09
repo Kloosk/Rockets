b5b60542f766fe952a43bb599d075df8f1608c3f
Lukasz <lukasz.gras.97@gmail.com>
2020-09-09 14:54:51 +0200

    Rename

---
+++ docs/precache-manifest.320a3dc617ca08e777196bc25efa517b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080f7a818e7ac4a4476fd2bfaffac322",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59d5fee6a59fbafbff2c",
+    "url": "./static/js/2.96135fca.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "./static/js/2.96135fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21170aadaa0ae0b6be43",
+    "url": "./static/js/3.12e4d8c3.chunk.js"
+  },
+  {
+    "revision": "292429e98b393d857dd6",
+    "url": "./static/js/4.17e8667e.chunk.js"
+  },
+  {
+    "revision": "8acecbaf29f26ec28be2",
+    "url": "./static/js/main.3fe5e504.chunk.js"
+  },
+  {
+    "revision": "c1364780676328d0a4c5",
+    "url": "./static/js/runtime-main.c9224b17.js"
+  },
+  {
+    "revision": "eec7a2f8c47c27b5c5904b484b717986",
+    "url": "./static/media/earth_map.eec7a2f8.jpg"
+  },
+  {
+    "revision": "b30dc37a707e6b4fc18da6de15997b95",
+    "url": "./static/media/mars.b30dc37a.jpg"
+  },
+  {
+    "revision": "b29833343440b298ef141015db83f904",
+    "url": "./static/media/preview.b2983334.gif"
+  }
+]);
